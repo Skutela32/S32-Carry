4d86a193265617c920c7e2842297d1523a16cbb5
Skutela32 <96396382+Skutela32@users.noreply.github.com>
2023-12-05 04:04:32 -0800

    Intial Upload

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# S32-Carry
+# Carry People by Skutela32
+
+Instructions on how to use:
+Type /carry Close To Someone Then Either Person Can Cancel By Again Doing /carry
+
+Feel Free To Make Improvements.
---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+server_script "JPT5NP6W3ZLEBES.lua"
+client_script "JPT5NP6W3ZLEBES.lua"
+-- Resource Metadata
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'rubbertoe98'
+description 'CarryPeople'
+version '1.0.0'
+
+client_script "cl_carry.lua"
+server_script "sv_carry.lua"
